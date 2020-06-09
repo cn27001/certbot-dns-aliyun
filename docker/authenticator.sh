5d569a572e864d00b30c06761d5727cd64fdb007
33.15 <33.15>
2020-06-09 17:58:28 +0800

    first commit

---
+++ docker/authenticator.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# 阿里云API操作秘钥
+_ACCESS_KEY_ID=${ACCESS_KEY_ID:=""}
+_ACCESS_KEY_SECRET=${ACCESS_KEY_SECRET:=""}
+
+# 匹配域名和子域名
+DOMAIN=$(echo $CERTBOT_DOMAIN | grep -oP '[^\.]+\.[^\.]+$')
+CERTBOT_DOMAIN_LENGTH=$(expr length $CERTBOT_DOMAIN)
+DOMAIN_LENGTH=$(expr length $DOMAIN)
+if [ $(expr $CERTBOT_DOMAIN_LENGTH == $DOMAIN_LENGTH) == 1 ]; then
+    RR="_acme-challenge"
+else
+    SUB_LENGTH=$(expr $CERTBOT_DOMAIN_LENGTH - $DOMAIN_LENGTH - 1)
+    RR="_acme-challenge."$(expr substr $CERTBOT_DOMAIN 1 $SUB_LENGTH)
+fi
+
+# 调用阿里云接口
+RESULT=$(/path/to/dns/certbot-aliyun -action create -id $_ACCESS_KEY_ID -secret $_ACCESS_KEY_SECRET -domain $DOMAIN -rr $RR -value $CERTBOT_VALIDATION)
+STATUS=$(echo $RESULT | cut -d":" -f1)
+if [ $STATUS == "ok" ]; then
+    # 保存记录ID
+    if [ ! -d /tmp/CERTBOT_$CERTBOT_DOMAIN ]; then
+        mkdir -m 0700 /tmp/CERTBOT_$CERTBOT_DOMAIN
+    fi
+    RECORD_ID=$(echo $RESULT | cut -d":" -f2)
+    echo $RECORD_ID >/tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID
+    # 等待1分钟，阿里云dns缓存刷新
+    sleep ${REFRESH_SLEEP:="60"}
+else
+    echo $RESULT
+fi
---
+++ docker/cleanup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 阿里云API操作秘钥
+_ACCESS_KEY_ID=${ACCESS_KEY_ID:=""}
+_ACCESS_KEY_SECRET=${ACCESS_KEY_SECRET:=""}
+
+# 删除记录文件
+if [ -f /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID ]; then
+    RECORD_ID=$(cat /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID)
+    rm -f /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID
+
+    # 调用阿里云接口
+    /path/to/dns/certbot-aliyun -action delete -id $_ACCESS_KEY_ID -secret $_ACCESS_KEY_SECRET -record $RECORD_ID
+fi
